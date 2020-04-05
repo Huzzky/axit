1fcee235690638d22887500418274a12f5e72dec
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-05 20:56:28 +0300

    master

--- src/components/main.js
+++ src/components/main.js
@@ -4,9 +4,24 @@ import "./css/main.css";
 class Main extends React.Component {
     render() {
         return (
-            <div>
-                
-                
+            <div className="All-MAin">
+                <div className="first-Main">
+                    <div className="left-div-first-main">
+                        <h1 className="axit-first"><b>AX</b><span className="it-first">IT</span></h1>
+                        <h2 className="about-axit-first">MODERN AXURE TEMPLATE FOR BEAUTIFUL PROTOTYPES</h2>
+                        <hr className="hr-first"></hr>
+                        <button className="button-first-download">Download</button>
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
         );}}
 
