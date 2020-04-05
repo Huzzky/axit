596277c035755508f61130c15107cbfb8f88dc9c
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-05 23:28:26 +0300

    master

--- src/components/main.js
+++ src/components/main.js
@@ -4,13 +4,22 @@ import "./css/main.css";
 class Main extends React.Component {
     render() {
         return (
-            <div className="All-MAin">
+            <div className="All-Main">
                 <div className="first-Main">
-                    <div className="left-div-first-main">
+                    <div className="div-first">
+                        <div className="left-div-first-main">
                         <h1 className="axit-first"><b>AX</b><span className="it-first">IT</span></h1>
-                        <h2 className="about-axit-first">MODERN AXURE TEMPLATE FOR BEAUTIFUL PROTOTYPES</h2>
+                        <h2 className="about-axit-first">LOREM IPSUM DOLOR SIT AMET CONSECTETUR ELIT</h2>
                         <hr className="hr-first"></hr>
-                        <button className="button-first-download">Download</button>
+                        <p className="fast-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla velit enim, 
+                        euismod nec purus et, 
+                        semper finibus sapien. Vivamus interdum, dui ut accumsan consectetur, 
+                        nec efficitur lorem odio. </p>
+                        <button className="button-first-download"><span className="download">Download</span></button>
+                    </div>
+                        <div className="card-main">
+
+                        </div>
                     </div>
                 </div>
                 <div></div>
